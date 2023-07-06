874349afe1434d98b1182bb5b06d3b6e22fd6eba
Ronan Lashermes <ronan.lashermes@inria.fr>
2023-07-06 15:51:57 +0200

    adding comments to idsl

--- idslpayload/src/main/scala/spinal/idslplugin/Annotation.scala
+++ idslpayload/src/main/scala/spinal/idslplugin/Annotation.scala
@@ -1,9 +1,17 @@
 package spinal.idslplugin
 
+// The `PostInitCallback` trait in the Scala compiler plugin is utilized to add additional steps to the initialization process of an object, specifically after the constructor has been called. 
+// Any class that extends this trait will have its constructor calls modified to include an invocation to a `postInitCallback` method.
+// This method is intended for executing tasks that can only be performed after an object's instantiation, allowing for more complex setup procedures that depend on runtime information.
+// The specific use of the `postInitCallback` will depend on the implementation within the class extending this trait.
 trait PostInitCallback {
-  def postInitCallback(): this.type
+    def postInitCallback(): this.type
 }
 
+// The `ValCallback` trait in the Scala compiler plugin is used to add custom behavior to `val` definitions within a class that uses this trait. 
+// When such a class is compiled, each `val` that isn't a parameter accessor and doesn't have a `DontName` annotation will have its right-hand side replaced with a call to a `valCallback` method. 
+// This method takes the original value of the `val` and its name as arguments. The exact nature of the custom behavior depends on the implementation of the `valCallback` method. 
+// This functionality can serve purposes like logging, dynamic behavior based on `val` values, or error checking and validation.
 trait ValCallback {
-  def valCallback[T](ref: T, name: String): T
+    def valCallback[T](ref: T, name: String): T
 }
--- idslplugin/src/main/scala/spinal/idslplugin/components/MainTransformer.scala
+++ idslplugin/src/main/scala/spinal/idslplugin/components/MainTransformer.scala
@@ -6,111 +6,126 @@ import scala.tools.nsc.Global
 import scala.tools.nsc.plugins.PluginComponent
 import scala.tools.nsc.transform.Transform
 
+// Define MainTransformer as a plugin component for the Scala compiler
 class MainTransformer(val global: Global) extends PluginComponent with Transform {
 
-  override val phaseName: String = "idsl-plugin"
+    override val phaseName: String = "idsl-plugin" // Set the phase name of this compiler plugin
 
-  override val runsAfter: List[String] = List("uncurry")
-  override val runsRightAfter: Option[String] = Some("uncurry")
+    // Define when this compiler plugin should run during the compilation process
+    override val runsAfter: List[String] = List("uncurry")
+    override val runsRightAfter: Option[String] = Some("uncurry")
 
-  override protected def newTransformer(unit: global.CompilationUnit): global.Transformer = ToStringMaskerTransformer
+    // The actual transformer object that will transform the abstract syntax tree (AST) during compilation
+    override protected def newTransformer(unit: global.CompilationUnit): global.Transformer = ToStringMaskerTransformer
 
-  import global._
+    import global._
 
+    // Define the transformer
+    object ToStringMaskerTransformer extends Transformer {
 
-  object ToStringMaskerTransformer extends Transformer {
+        // Helper function to check if a symbol has a specific annotation
+        def symbolHasAnnotation(s: Symbol, name: String): Boolean = {
+            if (s.annotations.exists(_.symbol.name.toString() == name)) return true
+            s.parentSymbols.exists(symbolHasAnnotation(_, name))
+        }
 
-    def symbolHasAnnotation(s: Symbol, name: String): Boolean = {
-      if (s.annotations.exists(_.symbol.name.toString() == name)) return true
-      s.parentSymbols.exists(symbolHasAnnotation(_, name))
-    }
-
-    def symbolHasTrait(s: Symbol, name: String): Boolean = {
-      s.parentSymbols.exists { p =>
-        (p.fullName == name) || symbolHasTrait(p, name)
-      }
-    }
-
-    def typeHasTrait(s: Type, name: String): Boolean = {
-      s.parents.exists { p =>
-        p.toString().toString == name  || typeHasTrait(p, name)
-      }
-    }
-
-
-    override def transform(tree: global.Tree): global.Tree = {
-      val transformedTree = super.transform(tree)
-      //      println(transformedTree.getClass.toString + " => \n" + transformedTree.toString)
-      transformedTree match {
-        case cd: ClassDef => {
-          var ret: Tree = cd
-
-          //No io bundle without component trait compilation time check
-          val withIoBundle = cd.impl.body.exists{
-            case vd : ValDef if vd.name.toString == "io " && vd.rhs != null && typeHasTrait(vd.rhs.tpe, "spinal.core.Bundle") => true
-            case _ => false
-          }
-          if(withIoBundle && !symbolHasTrait(cd.symbol, "spinal.core.Component") && !symbolHasTrait(cd.symbol, "spinal.core.Area" ) && !symbolHasTrait(cd.symbol, "spinal.core.Data" ) && !symbolHasTrait(cd.symbol, "spinal.core.AllowIoBundle" )){
-            global.globalError(cd.symbol.pos, s"MISSING EXTENDS COMPONENT\nclass with 'val io = new Bundle{...}' should extends spinal.core.Component")
-          }
-
-
-          //ValCallback management
-          if (symbolHasTrait(cd.symbol, "spinal.idslplugin.ValCallback")) {
-            val clazz = cd.impl.symbol.owner
-            val func = clazz.tpe.members.find(_.name.toString == "valCallback").get
-            val body = cd.impl.body.map {
-              case vd: ValDef if !vd.mods.isParamAccessor  && !vd.symbol.annotations.exists(_.symbol.name.toString == "DontName") && vd.rhs.nonEmpty =>
-                val nameStr = vd.getterName.toString
-                val const = Constant(nameStr)
-                val lit = Literal(const)
-                val thiz = This(clazz)
-                val sel = Select(thiz, func)
-                val appl = Apply(sel, List(vd.rhs, lit))
-
-                thiz.tpe = clazz.tpe
-                sel.tpe = func.tpe
-                appl.tpe = definitions.UnitTpe
-                lit.setType(definitions.StringTpe)
-                treeCopy.ValDef(vd, vd.mods, vd.name, vd.tpt, appl)
-              case e => e
+        // Helper function to check if a symbol or any of its parents has a specific trait
+        def symbolHasTrait(s: Symbol, name: String): Boolean = {
+            s.parentSymbols.exists { p =>
+                (p.fullName == name) || symbolHasTrait(p, name)
             }
-            val impl = treeCopy.Template(cd.impl, cd.impl.parents, cd.impl.self, body)
-            val cdNew = treeCopy.ClassDef(cd, cd.mods, cd.name, cd.tparams, impl) //)mods0, name0, tparams0, impl0
-
-            ret = cdNew
-          }
-
-          ret
         }
-        case a: Apply => {
-          var ret: Tree = a
 
-          if (a.fun.symbol.isConstructor) {
-            val sym = a.fun.symbol.enclClass
-            val tpe = sym.typeOfThis
-            if (symbolHasTrait(sym, "spinal.idslplugin.PostInitCallback")) {
-              val avoidIt = a match {
-                case Apply(Select(Super(_, _), _), _) => true
-                case Apply(Select(This(_), _), _) => true
-                case _ => false
-              }
-              if (!avoidIt) {
-                val func = tpe.members.find(_.name.toString == "postInitCallback").get
-                val sel = Select(a, func.name)
-                val appl = Apply(sel, Nil)
-                sel.tpe = MethodType(Nil, a.tpe)
-                appl.tpe = a.tpe
-                ret = appl
-              }
+        // Helper function to check if a type or any of its parent types has a specific trait
+        def typeHasTrait(s: Type, name: String): Boolean = {
+            s.parents.exists { p =>
+                p.toString().toString == name  || typeHasTrait(p, name)
             }
-          }
-          ret
         }
-        case oth => {
-          transformedTree
+
+        // The main transformation function that operates on the AST
+        override def transform(tree: global.Tree): global.Tree = {
+            val transformedTree = super.transform(tree) // Transform the tree using the parent's transform function
+            transformedTree match { // Match the transformed tree to specific cases
+                case cd: ClassDef => { // Case for class definitions
+                    var ret: Tree = cd
+
+                    // Check if the class contains a val definition for io of type spinal.core.Bundle
+                    val withIoBundle = cd.impl.body.exists{
+                            case vd : ValDef if vd.name.toString == "io " && vd.rhs != null && typeHasTrait(vd.rhs.tpe, "spinal.core.Bundle") => true
+                            case _ => false
+                    }
+
+                    // If the class contains io but does not extend spinal.core.Component or other specific traits, report a compilation error
+                    if(withIoBundle && !symbolHasTrait(cd.symbol, "spinal.core.Component") && !symbolHasTrait(cd.symbol, "spinal.core.Area" ) && !symbolHasTrait(cd.symbol, "spinal.core.Data" ) && !symbolHasTrait(cd.symbol, "spinal.core.AllowIoBundle" )){
+                            global.globalError(cd.symbol.pos, s"MISSING EXTENDS COMPONENT\nclass with 'val io = new Bundle{...}' should extends spinal.core.Component")
+                    }
+
+                    // If the class extends the trait spinal.idslplugin.ValCallback, manipulate the class body
+                    if (symbolHasTrait(cd.symbol, "spinal.idslplugin.ValCallback")) {
+                            val clazz = cd.impl.symbol.owner // Get the class symbol
+                            val func = clazz.tpe.members.find(_.name.toString == "valCallback").get // Find the valCallback function
+                            val body = cd.impl.body.map { // Map each element in the class body to a new element
+                                case vd: ValDef if !vd.mods.isParamAccessor  && !vd.symbol.annotations.exists(_.symbol.name.toString == "DontName") && vd.rhs.nonEmpty =>
+                                    // If the element is a val declaration, is not a parameter accessor, does not have the DontName annotation, and has a non-empty right hand side
+                                    val nameStr = vd.getterName.toString // Get the name of the getter
+                                    val const = Constant(nameStr) // Wrap it in a Constant
+                                    val lit = Literal(const) // Wrap the Constant in a Literal
+                                    val thiz = This(clazz) // Get the this reference for the class
+                                    val sel = Select(thiz, func) // Create a Select tree for the valCallback function
+                                    val appl = Apply(sel, List(vd.rhs, lit)) // Create an Apply tree for the valCallback function with the original rhs and the Literal as arguments
+
+                                    // Set the types for the new trees
+                                    thiz.tpe = clazz.tpe
+                                    sel.tpe = func.tpe
+                                    appl.tpe = definitions.UnitTpe
+                                    lit.setType(definitions.StringTpe)
+
+                                    // Replace the original val declaration with a new one that has the Apply tree as the rhs
+                                    treeCopy.ValDef(vd, vd.mods, vd.name, vd.tpt, appl)
+
+                                case e => e // For all other cases, just keep the tree element as is
+                            }
+
+                            val impl = treeCopy.Template(cd.impl, cd.impl.parents, cd.impl.self, body) // Create a new class body with the modified elements
+                            val cdNew = treeCopy.ClassDef(cd, cd.mods, cd.name, cd.tparams, impl) // Create a new class definition with the new class body
+
+                            ret = cdNew // Set ret to the new class definition
+                    }
+
+                    ret // Return the modified class definition
+                }
+                case a: Apply => { // Case for Apply trees
+                    var ret: Tree = a // Initialize ret
+
+                    if (a.fun.symbol.isConstructor) { // If the Apply is a constructor call
+                        val sym = a.fun.symbol.enclClass // Get the enclosing class of the constructor
+                        val tpe = sym.typeOfThis // Get the type of this for the enclosing class
+                        if (symbolHasTrait(sym, "spinal.idslplugin.PostInitCallback")) { // If the enclosing class extends the trait spinal.idslplugin.PostInitCallback
+                            val avoidIt = a match { // Check if the Apply is a constructor call for a superclass or for the current class
+                                case Apply(Select(Super(_, _), _), _) => true
+                                case Apply(Select(This(_), _), _) => true
+                                case _ => false
+                            }
+                            if (!avoidIt) { // If the Apply is not a constructor call for a superclass or for the current class
+                                val func = tpe.members.find(_.name.toString == "postInitCallback").get // Find the postInitCallback function
+                                val sel = Select(a, func.name) // Create a Select tree for the postInitCallback function
+                                val appl = Apply(sel, Nil) // Create an Apply tree for the postInitCallback function with no arguments
+
+                                // Set the types for the new trees
+                                sel.tpe = MethodType(Nil, a.tpe)
+                                appl.tpe = a.tpe
+
+                                ret = appl // Set ret to the new Apply tree
+                            }
+                        }
+                    }
+                    ret // Return the modified Apply tree
+                }
+                case oth => { // Case for other types of trees
+                    transformedTree // Just return the tree as it was transformed by the parent's transform function
+                }
+            }
         }
-      }
     }
-  }
 }
