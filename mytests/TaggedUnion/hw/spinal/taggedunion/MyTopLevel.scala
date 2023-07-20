e0dba893e7b40fc0320a3c7c0865d2dde17b686e
Ronan Lashermes <ronan.lashermes@inria.fr>
2023-07-20 16:53:19 +0200

    WIP

--- mytests/TaggedUnion/hw/spinal/taggedunion/MyTopLevel.scala
+++ mytests/TaggedUnion/hw/spinal/taggedunion/MyTopLevel.scala
@@ -3,89 +3,83 @@ package taggedunion
 import spinal.core._
 import spinal.lib._
 
-// case class ReadRequest() extends Bundle {
-//     val add = UInt(8 bits)
-// }
-
-// case class WriteRequest() extends Bundle {
-//     val add = UInt(8 bits)
-//     val data = Bits(32 bits)
-// }
-
-// case class ReadAnswer() extends Bundle {
-//     val data = Bits(32 bits)
-// }
-
-// case class WriteAnswer() extends Bundle {
-//     val ack = Bits(0 bits) // label has no hardware existence
-// }
-
-// object ReadWriteRequest extends TaggedUnion with IMasterSlave {
-//     // read or write -> TaggedUnion
-//     val read = ReadRequest()
-//     val write = WriteRequest()
-
-//     override def asMaster(): Unit = {
-//         out(read, write)
-//     }
-// }
-
-// object ReadWriteAnswer extends TaggedUnion with IMasterSlave{
-//     // read or write -> TaggedUnion
-//     val read = ReadAnswer()
-//     val write = WriteAnswer()
-
-//     override def asMaster(): Unit = {
-//         out(read, write)
-//     }
-// }
-
-// case class ReadWritePort() extends Bundle with IMasterSlave{
-//     // request and answer can be simultaneous -> Bundle
-//     val request = Stream(ReadWriteRequest())
-//     // answer with content only if read request
-//     val answer = Flow(ReadWriteAnswer())
-
-//     override def asMaster(): Unit = {
-//         master(request)
-//         slave(answer)
-//     }
-// }
-
-case class ReadPort() extends Bundle  {
-    val r1 = in Bits(8 bits)
-
+case class ReadRequest() extends Bundle {
+    val add = UInt(8 bits)
 }
 
-case class WritePort() extends Bundle {
-    val r2 = in Bits(5 bits)
-    val r3 = out Bits(3 bits)
+case class WriteRequest() extends Bundle {
+    val add = UInt(8 bits)
+    val data = Bits(32 bits)
 }
 
-object ReadWritePort extends TaggedUnion {
+case class ReadAnswer() extends Bundle {
+    val data = Bits(32 bits)
+}
+
+case class WriteAnswer() extends Bundle {
+    val ack = Bits(0 bits) // label has no hardware existence
+}
+
+object ReadWriteRequest extends TaggedUnion  {
     // read or write -> TaggedUnion
-    val read = ReadPort()
-    val write = WritePort()
+    val read = ReadRequest()
+    val write = WriteRequest()
+
+   
 }
+
+object ReadWriteAnswer extends TaggedUnion {
+    // read or write -> TaggedUnion
+    val read = ReadAnswer()
+    val write = WriteAnswer()
+}
+
+case class ReadWritePort() extends Bundle with IMasterSlave{
+    // request and answer can be simultaneous -> Bundle
+    val request = Stream(ReadWriteRequest.asMaster())
+    // answer with content only if read request
+    val answer = Flow(ReadWriteAnswer.asSlave())
+
+    override def asMaster(): Unit = {
+        master(request)
+        slave(answer)
+    }
+}
+
+// case class ReadPort() extends Bundle  {
+//     val r1 = in Bits(8 bits)
+
+// }
+
+// case class WritePort() extends Bundle {
+//     val r2 = in Bits(5 bits)
+//     val r3 = out Bits(3 bits)
+// }
+
+// object ReadWritePort extends TaggedUnion {
+//     // read or write -> TaggedUnion
+//     val read = ReadPort()
+//     val write = WritePort()
+// }
 
 case class MemoryController() extends Component {
     val io = new Bundle {
-        // val rwPort = ReadWritePort.asSlave()
-        val rwPort = ReadWritePort.asMaster()
-        val rw = in (Bool())
+        val rwPort = slave (ReadWritePort())
+        // val rwPort = ReadWritePort.asMaster()
+        // val rw = in (Bool())
     }
 
-    io.rwPort.default()
-    when(io.rw) {
-        io.rwPort.chooseOne("write") {
-            case write: WritePort => {
-                write.r3 := write.r2(2 downto 0)
-            }
-            case _ => {
-                SpinalInfo(s"other")
-            }
-        }
-    }
+    // io.rwPort.default()
+    // when(io.rw) {
+    //     io.rwPort.chooseOne("write") {
+    //         case write: WritePort => {
+    //             write.r3 := write.r2(2 downto 0)
+    //         }
+    //         case _ => {
+    //             SpinalInfo(s"other")
+    //         }
+    //     }
+    // }
 
     // val memory = Mem(Bits(32 bits), 256)
 
